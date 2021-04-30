9d0d44467b72162c4815f08372d67aa7d162f6ba
Cordial <alicia.cordial@laplateforme.io>
2021-04-30 16:58:47 +0200

    protection partie admin
    .htaccess
    .htpasswd

---
+++ pages/console.php
@@ -0,0 +1,12 @@
+CONSOLE
+SOIT ADMIN
+- gestion USER (inscription + suppression + status)
+- gestion AGENDA (ajout + suppression event + ajout users)
+- gestion CHAT (ajout pesonnes + différents chats)
+- TO DO LIST PERSO
+
+SOIT USER
+- update PASSWORD
+- TO DO LIST PERSO
+- accès au chat (ajout/suppression/modif message)
+- accès agenda
