47c23e3f22de8cf77dcc3452a34cfe818f787ce3
Cordial <alicia.cordial@laplateforme.io>
2021-07-17 20:34:56 +0200

    référencement

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 
   <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="Alicia Cordial">
+    <meta name="description" content="Jacquiod Entreprise est une entreprise d'électricité dans l'Ain à Priay">
+    <meta name="Jacquiod Électricité content='index,follow'">
   <!--Import Google Icon Font-->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <!-- Compiled and minified CSS -->
